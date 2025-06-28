077d22664391c7ab82b6e65e90ac74c788fa0055
ddukbaek2 <ddukbaek2@gmail.com>
2025-06-29 08:33:14 +0900

    샘플 추가.

---
+++ Samples~/Scripts/UIDesktopPanel.cs
@@ -0,0 +1,14 @@
+﻿using UIKit;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+/// <summary>
+/// 샘플 패널.
+/// </summary>
+[AssetPath("UI/UIExamplePanel")]
+public class UIExamplePanel : UIPanel
+{
+	#region INSPECTOR
+	#endregion
+}
---
+++ Samples~/Scripts/UIExampleState.cs
@@ -0,0 +1,54 @@
+﻿using UIKit;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+/// <summary>
+/// 샘플 화면.
+/// </summary>
+public class UIExampleState : UIModalState
+{
+	/// <summary>
+	/// 생성됨.
+	/// </summary>
+	public UIDesktopState() : base()
+	{
+	}
+
+	/// <summary>
+	/// 해제됨.
+	/// </summary>
+	protected override void OnDispose(bool explicitDisposing)
+	{
+		base.OnDispose(explicitDisposing);
+	}
+
+	/// <summary>
+	/// 뷰 로드 시작됨.
+	/// </summary>
+	protected override UIView OnViewLoadStart()
+	{
+		// 타이틀 패널 생성.
+		var view = UIView.CreateUIViewFromAsset<UIDesktopPanel>();
+		// var verticalLayoutGroupView = view.GetView("VerticalLayoutGroup");
+
+		// // 종료 버튼 등록.
+		// var exitButtonView = verticalLayoutGroupView.GetView("ExitButton");
+		// var button = exitButtonView.GetComponent<Button>();
+		// button.onClick.AddListener(OnClickExit);
+		return view;
+	}
+
+	/// <summary>
+	/// 종료 버튼 눌림.
+	/// </summary>
+	private void OnClickExit()
+	{
+		Debug.Log("UIDesktopState.OnClickExit()");
+#if UNITY_EDITOR
+		UnityEditor.EditorApplication.ExitPlaymode();
+#else
+		Application.Quit();
+#endif
+	}
+}
