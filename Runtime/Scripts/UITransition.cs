f5f22e87e77fc8dcd6c2b092ae63a7d8b961aa43
ddukbaek2 <ddukbaek2@gmail.com>
2025-07-18 05:50:28 +0900

    작업.

---
+++ Runtime/Scripts/UITransition.cs
@@ -0,0 +1,22 @@
+﻿namespace UIKit.Plugins.UIKit.Runtime.Scripts
+{
+	/// <summary>
+	/// 트랜지션.
+	/// </summary>
+	public class UITransition : Disposable
+	{
+		/// <summary>
+		/// 생성됨.
+		/// </summary>
+		public UITransition() : base()
+		{
+		}
+
+		/// <summary>
+		/// 해제됨.
+		/// </summary>
+		protected override void OnDispose(bool explicitDisposing)
+		{
+		}
+	}
+}
